f2eb758a3050117820f3ed8957ccd7de711a9e25
Shankar-pathade <109196454+Shankar-pathade@users.noreply.github.com>
2023-03-20 21:55:58 +0530

    Update Child.js

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-function Child() {
-    return <div id="child"></div>;
+function Child({age, incrementAge}) {
+  const handleClick = () => {
+    incrementAge();
+  }
+    return (
+      <div id="child">
+        <p id="text">Today I am {age} Years of Age</p>
+        <button id="button" onClick={handleClick}>Increment Age</button>
+      </div>
+    );
 }
 
 export default Child;
